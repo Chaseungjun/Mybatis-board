05788add2b9af15d42a604dddffd2c0c174ebe85
Seungjun Cha <chas369@naver.com>
2024-10-28 08:14:09 +0900

    feat: #16 커스텀 어노테이션을 생성, 둥록하고 미디어 타입을 처리할 수 있게 컨버터를 생성한다.
    * 1. 인증된 사용자의 userId를 자동으로 매개변수에 주입할 수 있게 커스텀 어노테이션을 생성하고 등록한다. (UserIdArgumentResolver, AuthenticatedUserId, WebConfig)
    * 2. 이미지를 등록할 때 미디어 타입을 처리할 수 있게 컨버터를 생성하고 등록한다. (MultipartJackson2HttpMessageConverter)

---
+++ module-api/src/main/java/com/study/api/blogUser/service/MultipartJackson2HttpMessageConverter.java
@@ -0,0 +1,35 @@
+package com.study.api.blogUser.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+
+@Component
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    /**
+     * "Content-Type: multipart/form-data" 헤더를 지원하는 HTTP 요청 변환기
+     * application/octet-stream 미디어 타입을 처리할 수 있는 컨버터를 생성
+     */
+    public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        return false;
+    }
+}
