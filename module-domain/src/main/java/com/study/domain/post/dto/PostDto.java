d3daa2380fda6b5c45c869b8c7d2c11dd4de26cb
Seungjun Cha <chas369@naver.com>
2024-10-28 05:33:14 +0900

    feat: #16 게시글의 등록, 반환에 관한 PostDto 클래스를 생성한다.
    * 1. 게시글 등록에 필요한 제목, 내용을 입력받는 PostRegisterDto 클래스
    * 2. 게시글에 이미지를 등록할 때 필요한 RegisterFileDto 클래스
    * 3. 게시글을 수정할 때 값을 입력받는 UpdatePostDto 클래스
    * 4. 게시글을 조회할 때 사용되는 PostResponse 클래스
    * 5. 게시글 리스트를 조회할 때 사용되는 PostListDto 클래스

---
+++ module-domain/src/main/java/com/study/domain/post/dto/PostDto.java
@@ -0,0 +1,92 @@
+package com.study.domain.post.dto;
+
+import com.study.domain.post.entity.Post;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Post에 대한 데이터 전송 객체(DTO)입니다.
+ */
+public class PostDto {
+
+    /**
+     * @param title   게시글 제목
+     * @param content 게시글 내용
+     */
+    public record PostRegisterDto(
+            @NotBlank(message = "제목을 입력해주세요") String title,
+            @NotBlank(message = "내용을 입력해주세요") String content
+    ) {
+    }
+
+    /**
+     * @param postId   게시글의 고유식별자 id
+     * @param fileUrls 게시글에 등록한 이미지 url 리스트
+     */
+    public record RegisterFileDto(
+            long postId,
+            List<String> fileUrls
+    ) {
+    }
+
+    /**
+     * @param title    게시글 제목
+     * @param content  게시글 내용
+     * @param fileUrls 게시글에 등록한 이미지 url 리스트
+     */
+    public record UpdatePostDto(
+            long postId,
+            @NotBlank(message = "제목을 입력해주세요") String title,
+            @NotBlank(message = "내용을 입력해주세요") String content,
+            List<String> fileUrls
+    ) {
+    }
+
+    /**
+     * @param postId  게시글 id
+     * @param title   게시글 제목
+     * @param content 게시글 내용
+     * @param userId  게시글을 등록한 유저의 ID
+     */
+    @Builder
+    public record PostResponse(
+            long postId,
+            String title,
+            String content,
+            String userId,
+            List<String> fileUrls
+
+    ) {
+
+        public static PostResponse fromPost(Post post, List<String> fileUrls) {
+            return PostResponse.builder()
+                    .postId(post.getId())
+                    .title(post.getTitle())
+                    .content(post.getContent())
+                    .fileUrls(fileUrls)
+                    .userId(post.getUserId())
+                    .build();
+        }
+    }
+
+    @Builder
+    public record PostListDto(
+            long postId,
+            String title,
+            String userId,
+            String nickName,
+            LocalDateTime createdDate
+    ) {
+        public static PostListDto of(Post post) {
+            return PostListDto.builder()
+                    .postId(post.getId())
+                    .title(post.getTitle())
+                    .userId(post.getUserId())
+                    .nickName(post.getNickName())
+                    .build();
+        }
+    }
+}
