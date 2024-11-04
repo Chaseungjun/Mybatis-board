2a6360d0028b9432f29271e4fb8b6d4665533b28
Seungjun Cha <chas369@naver.com>
2024-11-04 10:29:20 +0900

    feat: #30 Tag 엔티티, PostTag 엔티티 클래스를 생성한다.
    * 1. tag 클래스를 생성한다
    * 2. Post 와 Tag의 매핑 클래스 역할을 할 PostTag 클래스를 생성한다.

---
+++ module-domain/src/main/java/com/study/domain/tag/entity/Tag.java
@@ -0,0 +1,14 @@
+package com.study.domain.tag.entity;
+
+import lombok.Getter;
+
+/**
+ * 게시글 태그 나타내는 엔티티 클래스입니다.
+ */
+@Getter
+public class Tag {
+
+    private int id;
+    private String tagName;
+
+}
